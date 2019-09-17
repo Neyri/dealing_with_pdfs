6fb7879a6d018adb00ee3354f7f7a9a47f915e31
Martin CHAUVIN <martin.chauvin@capgemini.com>
2019-09-17 10:00:21 +0200

    Fixing some bug

--- extract_pages.py
+++ extract_pages.py
@@ -5,8 +5,8 @@ import PyPDF2
 def get_pages_number(input_nb):
     pages_nb = []
     for i in range(len(input_nb)):
-        if '-' in input_nb[i]:
-            min, max = input_nb[i].split('-')
+        if "-" in input_nb[i]:
+            min, max = input_nb[i].split("-")
             min = int(min)
             max = int(max)
             pages_nb += [i for i in range(min, max + 1)]
@@ -16,7 +16,7 @@ def get_pages_number(input_nb):
 
 
 def extract_pages(pages_nb, file_name):
-    input_file = PyPDF2.PdfFileReader(file_name)
+    input_file = PyPDF2.PdfFileReader(file_name, strict=False)
     pages = []
     for page_nb in pages_nb:
         page = input_file.getPage(page_nb - 1)
@@ -28,8 +28,8 @@ def write_file(pages, output_name):
     new_file = PyPDF2.PdfFileWriter()
     for page in pages:
         new_file.addPage(page)
-    output_file = open(output_name, 'wb')
-    new_file.write(output_file)
+    with open(output_name, "wb") as output_file:
+        new_file.write(output_file)
 
 
 def main():
@@ -42,12 +42,12 @@ def main():
         input_name = input("Name of the file from which to extract pages: ")
         input_nb = input("Pages to extract (in format 2;3;7-10): ")
         output_name = input("Name of the new file to write: ")
-    input_nb = input_nb.split(';')
+    input_nb = input_nb.split(";")
     pages_nb = get_pages_number(input_nb)
     print("... Extracting pages : ", pages_nb)
     pages = extract_pages(pages_nb, input_name)
     write_file(pages, output_name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
